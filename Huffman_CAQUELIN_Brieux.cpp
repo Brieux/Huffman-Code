b0cb9e4beaed9eab40d26bf1d312ec51eeb91533
HNS <72392569+hns707@users.noreply.github.com>
2021-04-07 13:00:47 +0200

    count

--- Huffman_CAQUELIN_Brieux.cpp
+++ Huffman_CAQUELIN_Brieux.cpp
@@ -1,29 +1,33 @@
-// Huffman_CAQUELIN_Brieux.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
-
 #include <iostream>
 #include <fstream>
+#include <map>
 
 using namespace std;
+<<<<<<< Updated upstream
+=======
+
+
+>>>>>>> Stashed changes
 
 int main()
 {
     fstream file;
     file.open("texteDeChanson.txt");
+    map<char, int> cTab;
     char c;
     while (file.get(c)) {
-        cout << c;
+        map<char, int>::iterator it = cTab.find(c);
+        if (it == cTab.end()) {
+            cTab.insert({c,1});
+        }
+        else {
+            it->second += 1;
+        }       
     }
+
+    for (const auto& p : cTab) {
+        std::cout << "Char [" << p.first << "] = " << p.second << " iterations." << endl;
+    }
+
     return 0;
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
