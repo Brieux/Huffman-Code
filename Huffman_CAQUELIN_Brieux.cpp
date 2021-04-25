32c4a96417e9a8b0c7573d7c5880e13ef0bf5edf
Brieux "Thazerr" Caquelin <33527111+Brieux@users.noreply.github.com>
2021-04-25 20:42:01 +0200

    Question 6 c'est bon

--- Huffman_CAQUELIN_Brieux.cpp
+++ Huffman_CAQUELIN_Brieux.cpp
@@ -55,24 +55,27 @@ int main()
     cout << endl << endl;*/
     sortWithFrequency(tree.leaf);
 
-    for (int i = 0; i < tree.leaf.size(); i++) {
+    /*for (int i = 0; i < tree.leaf.size(); i++) {
         cout << tree.leaf[i].getValue() << " & " << tree.leaf[i].getFrequency() << endl;
-    }
+    }*/
+    for (int j = 0; j < tree.leaf.size() - 1; j++) {
+        for (int i = tree.leaf.size() - 1; i > 1; i = i - 2) {
+            Node l1(tree.leaf[i].getValue(), tree.leaf[i].getFrequency());
+            tree.leaf.pop_back();
+            Node l2(tree.leaf[i - 1].getValue(), tree.leaf[i - 1].getFrequency());
+            tree.leaf.pop_back();
+            Node branch('|', l1.getFrequency() + l2.getFrequency());
+            branch.setChild('l', l2);
+            branch.setChild('r', l1);
+            tree.leaf.push_back(branch);
+            sortWithFrequency(tree.leaf);
+        }
 
-    for (int i = tree.leaf.size() - 1; i > 1; i = i - 2) {
-        Node l1(tree.leaf[i].getValue(), tree.leaf[i].getFrequency());
-        tree.leaf.pop_back();
-        Node l2(tree.leaf[i - 1].getValue(), tree.leaf[i - 1].getFrequency());
-        tree.leaf.pop_back();
-        Node branch('§',l1.getFrequency() + l2.getFrequency());
-        branch.setChild('l', l2);
-        branch.setChild('r', l1);
-        tree.leaf.push_back(branch);
+        for (int i = 0; i < tree.leaf.size(); i++) {
+            cout << tree.leaf[i].getValue() << " & " << tree.leaf[i].getFrequency() << endl;
+        }
     }
-
-    for (int i = 0; i < tree.leaf.size(); i++) {
-        cout << tree.leaf[i].getValue() << " & " << tree.leaf[i].getFrequency() << endl;
-    }
-    cout << tree.leaf[2].getChild('l')->getValue() << endl;
+    /*
+    cout << tree.leaf[1].getChild('r')->getValue() << endl;*/
     return 0;
 }
