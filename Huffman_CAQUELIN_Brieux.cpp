62dfb06853350807b587d236e0a6cce404b41cb1
Brieux "Thazerr" Caquelin <33527111+Brieux@users.noreply.github.com>
2021-04-25 19:27:07 +0200

    trie des Nodes juste

--- Huffman_CAQUELIN_Brieux.cpp
+++ Huffman_CAQUELIN_Brieux.cpp
@@ -1,15 +1,31 @@
-#include <iostream>
 #include <fstream>
 #include <map>
 #include <vector>
 #include <algorithm> 
-#include "Node.cpp"
+#include "Node.h"
 
 using namespace std;
 
 struct tree {
     vector<Node> leaf;
-}   arbres;
+} tree;
+
+void sortWithFrequency(vector<Node> &v) {
+    for (int j = 0; j < v.size() - 1; j++) {
+        for (int i = 0; i < v.size() - j - 1; i++) {
+
+
+            if (v[i].getFrequency() < v[i + 1].getFrequency()) {
+                Node tampon(v[i].getValue(), v[i].getFrequency());
+                v[i].setValue(v[i + 1].getValue());
+                v[i].setFrenquency(v[i + 1].getFrequency());
+                v[i + 1].setValue(tampon.getValue());
+                v[i + 1].setFrenquency(tampon.getFrequency());
+
+            }
+        }
+    }
+}
 
 int main()
 {
@@ -17,7 +33,6 @@ int main()
     file.open("texteDeChanson.txt");
     map<char, int> cTab;
     char c;
-    vector<int> nbrIte;
 
     while (file.get(c)) {
         map<char, int>::iterator it = cTab.find(c);
@@ -28,27 +43,21 @@ int main()
             it->second += 1;
         }       
     }
-    
-    cout << "Non tri" << endl;
 
     for (const auto& p : cTab) {
-
-        cout << "Char [" << p.first << "] = " << p.second << " iterations." << endl;
-        nbrIte.push_back(p.second);
         Node l(p.first, p.second);
-
-
-        arbres.leaf.push_back(l); //ajout de la feuille dans l'arbres  
+        tree.leaf.push_back(l); //ajout de la feuille dans l'arbres    
     }
+    /*
+    for (int i = 0; i < tree.leaf.size(); i++) {
+        cout << tree.leaf[i].getValue() << " & " << tree.leaf[i].getFrequency() << endl ;
+    }
+    cout << endl << endl;*/
+    sortWithFrequency(tree.leaf);
 
-    sort(nbrIte.begin(), nbrIte.end(), greater<int>());
-    
-    cout << "tri" << endl;
-
-    for (auto x : nbrIte)
-        cout << x << " ";
-
-
+    for (int i = 0; i < tree.leaf.size(); i++) {
+        cout << tree.leaf[i].getValue() << " & " << tree.leaf[i].getFrequency() << endl;
+    }
 
     return 0;
 }
