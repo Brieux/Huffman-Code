e59c16c3790e935ebea03f5fffa14b7a9230acc7
Brieux "Thazerr" Caquelin <33527111+Brieux@users.noreply.github.com>
2021-04-27 12:34:06 +0200

    Suppresion de la struct tree pour juste un vector sur *Node

--- Huffman_CAQUELIN_Brieux.cpp
+++ Huffman_CAQUELIN_Brieux.cpp
@@ -6,26 +6,22 @@
 
 using namespace std;
 
-struct tree {
-    vector<Node> leaf;
-} tree;
-
 struct translate {
     char character;
     int binary;
 };
 
-void sortWithFrequency(vector<Node> &v) {
+void sortWithFrequency(vector<Node *> v) {
     for (int j = 0; j < v.size() - 1; j++) {
         for (int i = 0; i < v.size() - j - 1; i++) {
 
 
-            if (v[i].getFrequency() < v[i + 1].getFrequency()) {
-                Node tampon(v[i].getValue(), v[i].getFrequency());
-                v[i].setValue(v[i + 1].getValue());
-                v[i].setFrenquency(v[i + 1].getFrequency());
-                v[i + 1].setValue(tampon.getValue());
-                v[i + 1].setFrenquency(tampon.getFrequency());
+            if (v[i]->getFrequency() < v[i + 1]->getFrequency()) {
+                Node tampon(v[i]->getValue(), v[i]->getFrequency());
+                v[i]->setValue(v[i + 1]->getValue());
+                v[i]->setFrenquency(v[i + 1]->getFrequency());
+                v[i + 1]->setValue(tampon.getValue());
+                v[i + 1]->setFrenquency(tampon.getFrequency());
 
             }
         }
@@ -38,6 +34,7 @@ int main()
     file.open("texteDeChanson.txt");
     map<char, int> cTab;
     char c;
+    vector<Node *> leaf;
 
     while (file.get(c)) {
         map<char, int>::iterator it = cTab.find(c);
@@ -50,30 +47,35 @@ int main()
     }
 
     for (const auto& p : cTab) {
-        Node l(p.first, p.second);
-        tree.leaf.push_back(l); //ajout de la feuille dans l'arbres    
+        cout << p.first << " " << p.second << endl;
+        leaf.push_back(new Node(p.first, p.second)); //ajout de la feuille dans l'arbres
+
     }
-
-    sortWithFrequency(tree.leaf);
-
-
-    for (int j = 0; j < tree.leaf.size(); j++) {
-        for (int i = tree.leaf.size() - 1; i > 1; i = i - 2) {
-            Node l1(tree.leaf[i].getValue(), tree.leaf[i].getFrequency());
-            tree.leaf.pop_back();
-            Node l2(tree.leaf[i - 1].getValue(), tree.leaf[i - 1].getFrequency());
-            tree.leaf.pop_back();
+    cout << "TRI" << endl;
+    sortWithFrequency(leaf);
+    for (int i = 0; i <leaf.size(); i++) {
+        cout << leaf[i]->getValue() << " & " << leaf[i]->getFrequency() << endl;
+    }
+    /*
+    for (int j = 0; j < leaf.size(); j++) {
+        for (int i = leaf.size() - 1; i > 1; i = i - 2) {
+            Node l1(leaf[i]->getValue(), leaf[i]->getFrequency());
+            leaf.pop_back();
+            Node l2(leaf[i - 1]->getValue(), leaf[i - 1]->getFrequency());
+            leaf.pop_back();
             Node branch('|', l1.getFrequency() + l2.getFrequency());
             branch.setChild('l', l2);
             branch.setChild('r', l1);
-            tree.leaf.push_back(branch);
-            sortWithFrequency(tree.leaf);
+            leaf.push_back(&branch);
+            sortWithFrequency(leaf);
         }
         cout << "Tour " << j << "_______________________" << endl;
-        for (int i = 0; i < tree.leaf.size(); i++) {
-            cout << tree.leaf[i].getValue() << " & " << tree.leaf[i].getFrequency() << endl;
+        for (int i = 0; i < leaf.size(); i++) {
+            cout << leaf[i]->getValue() << " & " << leaf[i]->getFrequency() << endl;
         }
     }
+
+    cout << leaf[1]->getChild('r')->getValue();*/
 
     return 0;
 }
